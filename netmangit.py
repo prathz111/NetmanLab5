3bf256848afa40c829f6056c18d17438f50e53a9
netman <netman@csci5180-prsh2459.int.colorado.edu>
2023-03-06 16:00:52 -0700

    Modified files

--- netmangit.py
+++ netmangit.py
@@ -1,74 +1,53 @@
 import os
 import requests
-import shutil
 
 # Set the GitHub repository URL
-repo_url = "https://github.com/prathz111/NetmanLab5"
+repo_url = "https://api.github.com/repos/prathz111/NetmanLab5"
 
 # Set the access token
 access_token = "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"
 
-# Set the local directory to clone the repository
+# Create the headers for the API request
+headers = {"Authorization": "Bearer " + access_token, "Accept": "application/vnd.github.v3+json"}
+
+# Get the latest commit SHA for the default branch
+response = requests.get(repo_url + "/commits", headers=headers)
+response.raise_for_status()
+sha = response.json()[0]["sha"]
+print("hhhh")
+# Set the local directory name
 local_dir = "/home/netman/Documents/Lab5Midterm/Lab5_python"
 
-# Create a directory if it doesn't exist
-if not os.path.exists(local_dir):
-    os.makedirs(local_dir)
-    print("Created directory:", local_dir)
-
 # Clone the GitHub repository if it doesn't exist locally
-if not os.path.exists(os.path.join(local_dir, ".git")):
-    os.chdir(local_dir)
-    os.system("git init")
-    os.system("git remote add origin {}".format(repo_url))
-    os.system("git fetch")
-    os.system("git checkout {}".format("main"))
-    print("Cloned repository:", repo_url)
+if not os.path.exists(local_dir):
+    os.system("git clone " + repo_url + ".git " + local_dir)
+
+# Change directory to the local directory
+os.chdir(local_dir)
 
 # Add the .txt and .jpg files to the staging area
-os.chdir(local_dir)
-os.system("touch file.txt")
-os.system("touch file.jpg")
-os.system("git add .")
-print("Added files to staging area")
+os.system("git add file.txt file.jpg")
 
 # Commit the changes to the local repository
-os.system('git commit -m "Added files"')
-print("Committed changes to local repository")
+os.system("git commit -m 'Added files'")
 
 # Push the changes to the GitHub repository
-os.system("git push origin {}".format("main"))
-print("Pushed changes to remote repository")
+os.system("git push " + repo_url + " HEAD:master")
 
 # Get the latest changes from the GitHub repository
-response = requests.get("https://api.github.com/repos/prathz111/NetmanLab5/commits?per_page=1&access_token=" + access_token)
-sha = response.json()[0]["sha"]
-url = "https://github.com/prathz111/NetmanLab5/archive/{}.zip".format(sha)
-response = requests.get(url, stream=True)
-with open("repo.zip", "wb") as f:
-    shutil.copyfileobj(response.raw, f)
-print("Downloaded latest changes from GitHub repository")
+os.system("git pull " + repo_url)
 
-# Extract the downloaded repository zip file
-shutil.unpack_archive("repo.zip", local_dir)
-print("Extracted downloaded zip file to:", local_dir)
-
-# Check for differences between the local and remote repositories
-os.chdir(local_dir)
-diff = os.system("git diff origin/main")
-if diff != 0:
-    print("There are differences between local and remote repositories")
-else:
-    print("No differences between local and remote repositories")
+# Compare the modified files in the local repository against the GitHub repository
+os.system("git diff " + sha)
 
 # Add the modified files to the staging area
 os.system("git add .")
-print("Added modified files to staging area")
 
 # Commit the changes to the local repository
-os.system('git commit -m "Modified files"')
-print("Committed changes to local repository")
+os.system("git commit -m 'Modified files'")
 
 # Push the changes to the GitHub repository
-os.system("git push origin {}".format("main"))
-print("Pushed changes to remote repository")
+os.system("git push " + repo_url + " HEAD:master")
+
+# Print that the push was successful
+print("Pushed changes to remote repository.")
