67e38611f8da7d6e6771e5ede2416ccd3e13997c
netman <netman@csci5180-prsh2459.int.colorado.edu>
2023-03-06 20:52:59 -0700

    Update from Python script

--- netmangit.py
+++ netmangit.py
@@ -1,7 +1,7 @@
 import git
 
 # Set the token secret
-token_secret = "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"
+token_secret = "YOUR_TOKEN_SECRET_HERE"
 
 # Initialize a Git repository object
 repo = git.Repo()
@@ -9,13 +9,14 @@ repo = git.Repo()
 # Add all files in the current directory to the staging area
 repo.git.add(".")
 
-# Check if there are any changes to commit
-if repo.is_dirty():
-    # Commit the changes with a message
-    commit_msg = "Update from Python script"
-    repo.git.commit("-m", commit_msg)
-else:
-    print("No changes to commit.")
+# Commit the changes with a message
+commit_msg = "Update from Python script"
+repo.git.commit("-m", commit_msg)
+
+# Push the changes to the remote repository with the token secret
+remote_url = repo.remote().url
+remote_url_with_secret = remote_url.replace("://", "://" + token_secret + "@")
+repo.git.push(remote_url_with_secret)
 
 # Function to compare modified files in local repository with GitHub repository
 def compare_files():
