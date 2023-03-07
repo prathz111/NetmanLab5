371b317a4eed4d900ef03e2257135e8b97ed4b8c
netman <netman@csci5180-prsh2459.int.colorado.edu>
2023-03-06 21:12:51 -0700

    Update from Python script

--- netmangit.py
+++ netmangit.py
@@ -1,35 +1,38 @@
-import os
 import git
 
-print("llllllllll")
-
-# Set the access token
-access_token = "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"
-
-# Set the Git askpass helper environment variable to avoid being prompted for a password
-os.environ["GIT_ASKPASS"] = "git_askpass_helper"
+# Set the token secret and GitHub repository URL
+token_secret = "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"
+github_repo_url = "https://github.com/prathz111/NetmanLab5"
 
 # Initialize a Git repository object
 repo = git.Repo()
 
-# Add all files in the current directory to the staging area
-repo.git.add(".")
-# Commit the changes with a message
-commit_msg = "Update from Python script"
-repo.git.commit("-m", commit_msg)
+def add_and_commit_changes(commit_msg):
+    # Add all files in the current directory to the staging area
+    repo.git.add(".")
+    # Commit the changes with a message
+    repo.git.commit("-m", commit_msg)
 
-# Push the changes to the remote repository with the access token
-remote_url = repo.remote().url
-remote_url_with_token = remote_url.replace("://", "://{}@".format(access_token))
-repo.git.push(remote_url_with_token)
+def push_changes():
+    # Push the changes to the remote repository with the token secret
+    remote_url = repo.remote().url
+    remote_url_with_secret = remote_url.replace("://", "://{}@".format(token_secret))
+    repo.git.push(remote_url_with_secret)
 
-# Compare modified files in the local repository against the GitHub repository and print the difference
-def compare_files():
-    remote = repo.remote()
-    remote.fetch()
-    local_diff = repo.git.diff("--name-only", "--diff-filter=M").splitlines()
-    remote_diff = repo.git.diff("--name-only", "--diff-filter=M", "{}/HEAD".format(remote.name)).splitlines()
-    print("Modified files in local repository but not in remote repository:")
-    print(set(local_diff) - set(remote_diff))
-
-compare_files()
+def compare_and_push_changes():
+    # Fetch the latest changes from the remote repository
+    repo.remotes.origin.fetch()
+    # Compare modified files in the local repository against the GitHub repository and print the difference
+    local_changes = set([item.a_path for item in repo.index.diff(None)])
+    remote_changes = set([item.a_path for item in repo.index.diff("origin/{}".format(repo.active_branch.name))])
+    changes_to_push = local_changes.intersection(remote_changes)
+    if changes_to_push:
+        print("The following modified files will be pushed to GitHub:")
+        for filename in changes_to_push:
+            print("- {}".format(filename))
+        add_and_commit_changes("Update from Python script")
+        push_changes()
+    else:
+        print("There are no modified files to push to GitHub.")
+# Call the compare_and_push_changes function
+compare_and_push_changes()
