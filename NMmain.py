15007f5b4d2085026c0792fa90786fec807032a4
netman <netman@csci5180-prsh2459.int.colorado.edu>
2023-03-06 22:43:02 -0700

    Update from Python script

--- NMmain.py
+++ NMmain.py
@@ -4,10 +4,10 @@ from NMsnmp import *
 from NMgithub import *
 
 def main():
-    run_NMsnmp()
+    # run_NMsnmp()
     run_NMtcpdump()
-    run_NMgithub()
-    run_NMdhcpserver()
+    # run_NMgithub()
+    # run_NMdhcpserver()
 
 if __name__ == '__main__':
     main()
